5fad00a1753b4be83f8894f263f5db079acf39bf
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-10 04:35:07 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
+require 'pry'
 def unsafe?(speed)
+  if speed >= 40 && speed <= 60
+    return false
+  elsif speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+
+  end
+
 
 end
 
 
 
 def not_safe?(speed)
-	
+  #binding.pry
+  speed < 40 || speed > 60 ? true:false
+
 end
-	
-
-
